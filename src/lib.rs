2117bcc8454367aca3f3e1c552377ed0eb4ab9ea
xuzhuDing <texuepeng@gmial.com>
2025-04-13 20:42:27 +0800

    first commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,1 @@
-// reference other 
-mod opts;
-mod process;
-
-pub use opts::{Opts, Subcommand};
-pub use process::process_csv;
+// reference other
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use clap::Parser;
-use rcli::{process_csv, Opts, Subcommand};
+fn main() {
+    let x = 1;
+    let y = 2;
+    let z = add(x, y);
+    print!("Hello, world!{}", z);
+}
 
-fn main() -> anyhow::Result<()> {
-    let opts: Opts = Opts::parse();
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+#[cfg(test)]
+mod tests {
+    // 注意这个惯用法：在 tests 模块中，从外部作用域导入所有名字。
+    use super::*;
 
-    match opts.cmd {
-        Subcommand::CSV(opts) => {
-            process_csv(&opts.input, &opts.output)?
-        }
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
     }
-
-    Ok(())
 }
